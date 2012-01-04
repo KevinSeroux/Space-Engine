1e7415fca2fc1d21a54be4845aa01e9f05ccbcde
Devuxx <kevin.seroux@orange.fr>
2012-01-04 18:50:27 +0100

    Using template for VarManager

--- include/VarManager.h
+++ include/VarManager.h
@@ -1,9 +1,19 @@
+#ifndef VARMANAGER_H
+#define VARMANAGER_H
+
 #include <string>
 #include <sstream>
 
 class VarManager
 {
  public:
-    template <class T>
-    static std::string& convertToString(const T& str);
+    template<class T>
+    static const std::string& convertToString(const T& str)
+    {
+	std::ostringstream oss;
+	oss << str;
+	return oss.str();
+    }
 };
+
+#endif
--- src/GameEngine/VarManager.cpp
+++ src/GameEngine/VarManager.cpp
@@ -1,9 +1,1 @@
 #include "VarManager.h"
-
-template <class T>
-std::string& VarManager::convertToString(const T& str)
-{
-    std::ostringstream oss;
-    oss << str;
-    return oss.str();
-}
